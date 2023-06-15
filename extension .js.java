64850d9d5f9a62e3f2c86ee42d3d47c630c515cd
Anllupi <132388022+Anllupi@users.noreply.github.com>
2023-06-15 13:12:00 +0200

    Add files via upload

---
+++ extension .js.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class Program {
+    public static void main(string[]args)
+    {
+        scanner sc = new
+        scanner(system.in);
+        double bill = sc.nextDouble();
+        System.out.print(bill * 50 / 100);
+    }
+}
